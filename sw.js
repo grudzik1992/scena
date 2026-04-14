3964b68a69bfa761c69061e9d3278a53d2edd877
grudzik1992 <a.grudzinski1992@gmail.com>
2026-04-14 13:00:15 +0200

    feat: add PWA, bundled songs.json, and rich formatting editor

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "spiewnik-pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./koncert.html",
+  "./songs.json",
+  "./utwory.txt",
+  "./manifest.webmanifest",
+  "./icons/icon-192.svg",
+  "./icons/icon-512.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  if (req.mode === "navigate") {
+    event.respondWith(
+      fetch(req).catch(() => caches.match("./koncert.html"))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
+      return res;
+    }))
+  );
+});
